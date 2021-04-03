1fb61bf18f8c2d1fe976bd86a41c7ab44b293d26
dododong76 <dododong76@gmail.com>
2021-04-03 17:27:12 +0900

    '0403'

--- script.js
+++ script.js
@@ -1,213 +1,233 @@
-"use strict";
+/**
+ * Debug
+ */
+const gui = new dat.GUI({ closed: false, width: 340 });
+const bigWavesFolder = gui.addFolder("Large Waves");
+const smallWavesFolder = gui.addFolder("Small Waves");
+const colorFolder = gui.addFolder("Colors");
+const debugObject = {
+  waveDepthColor: "#1e4d40",
+  waveSurfaceColor: "#4d9aaa",
+  fogNear: 1,
+  fogFar: 3,
+  fogColor: "#8e99a2" };
 
-window.addEventListener("load",function() {
 
-  let canv, ctx;    // canvas and context
-  let maxx, maxy;   // canvas dimensions
-  let c;
-  let hue;
+/**
+ * Base
+ */
+// Canvas
+const canvas = document.querySelector("canvas.webgl");
 
-// for animation
-  let events;
+// Scene
+const scene = new THREE.Scene();
+scene.fog = new THREE.Fog(
+debugObject.fogColor,
+debugObject.fogNear,
+debugObject.fogFar);
 
-// shortcuts for Math.
-  const mrandom = Math.random;
-  const mfloor = Math.floor;
-  const mround = Math.round;
-  const mceil = Math.ceil;
-  const mabs = Math.abs;
-  const mmin = Math.min;
-  const mmax = Math.max;
+scene.background = new THREE.Color(debugObject.fogColor);
 
-  const mPI = Math.PI;
-  const mPIS2 = Math.PI / 2;
-  const mPIS3 = Math.PI / 3;
-  const m2PI = Math.PI * 2;
-  const m2PIS3 = Math.PI * 2 / 3;
-  const msin = Math.sin;
-  const mcos = Math.cos;
-  const matan2 = Math.atan2;
+/**
+ * Object
+ */
+const waterGeometry = new THREE.PlaneGeometry(12, 12, 512, 512);
 
-  const mhypot = Math.hypot;
-  const msqrt = Math.sqrt;
+// Material
+const waterMaterial = new THREE.ShaderMaterial({
+  vertexShader: document.getElementById("vertexShader").textContent,
+  fragmentShader: document.getElementById("fragmentShader").textContent,
+  transparent: true,
+  fog: true,
+  uniforms: {
+    uTime: { value: 0 },
+    uMouse: { value: new THREE.Vector2() },
+    uBigWavesElevation: { value: 0.2 },
+    uBigWavesFrequency: { value: new THREE.Vector2(4, 2) },
+    uBigWaveSpeed: { value: 0.75 },
+    // Small Waves
+    uSmallWavesElevation: { value: 0.15 },
+    uSmallWavesFrequency: { value: 3 },
+    uSmallWavesSpeed: { value: 0.2 },
+    uSmallWavesIterations: { value: 4 },
+    // Color
+    uDepthColor: { value: new THREE.Color(debugObject.waveDepthColor) },
+    uSurfaceColor: { value: new THREE.Color(debugObject.waveSurfaceColor) },
+    uColorOffset: { value: 0.08 },
+    uColorMultiplier: { value: 5 },
 
-  const rac3   = msqrt(3);
-  const rac3s2 = rac3 / 2;
+    // Fog, contains fogColor, fogDensity, fogFar and fogNear
+    ...THREE.UniformsLib["fog"] } });
 
-//------------------------------------------------------------------------
 
-function alea (mini, maxi) {
-// random number in given range
 
-  if (typeof(maxi) == 'undefined') return mini * mrandom(); // range 0..mini
+const water = new THREE.Mesh(waterGeometry, waterMaterial);
+water.rotation.x = -Math.PI * 0.5;
+scene.add(water);
 
-  return mini + mrandom() * (maxi - mini); // range mini..maxi
-}
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-function intAlea (mini, maxi) {
-// random integer in given range (mini..maxi - 1 or 0..mini - 1)
-//
-  if (typeof(maxi) == 'undefined') return mfloor(mini * mrandom()); // range 0..mini - 1
-  return mini + mfloor(mrandom() * (maxi - mini)); // range mini .. maxi - 1
-}
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+/**
+ * Sizes
+ */
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight };
 
-function Noise1DOneShot (period, min = 0, max = 1, random) {
-/* returns a 1D single-shot noise generator.
-   the (optional) random function must return a value between 0 and 1
-  the returned function has no parameter, and will return a new number every tiime it is called.
-  If the random function provides reproductible values (and is not used elsewhere), this
-  one will return reproductible values too.
-  period should be > 1. The bigger period is, the smoother output noise is
-*/
-  random = random || Math.random;
-  let currx = random(); // start with random offset
-  let y0 = min + (max - min) * random(); // 'previous' value
-  let y1 = min + (max - min) * random(); // 'next' value
-  let dx = 1 / period;
 
-  return function() {
-    currx += dx;
-    if (currx > 1) {
-      currx -= 1;
-      y0 = y1;
-      y1 = min + (max - min) * random();
-    }
-    let z = (3 - 2 * currx) * currx * currx;
-    return z * y1 + (1 - z) * y0;
-  }
-} // Noise1DOneShot
+window.addEventListener("resize", () => {
+  // Update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
 
-//------------------------------------------------------------------------
+  // Update camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
 
-function Circle(R, level) {
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
 
-  this.level = level;
-  this.R = R;
+/**
+ * Camera
+ */
 
-  if (level & 1)
-    this.hue0 = false;
-  else {
-    this.fdLum = Noise1DOneShot(intAlea(300,500), 20, 80);
-  }
-  this.children = [];
-  if (level <= 3) {
-    this.th0 = alea(m2PI);
-    this.fdth = Noise1DOneShot(intAlea(300,600), -1 / R, 1 / R);
-    this.n = intAlea(((level == 0) ? 3 : 2), 7);
-    this.r = R / (1 + 1 / msin(mPI / this.n));
-    this.dr = R - this.r;
-    for (let k = 0; k < this.n; ++k) {
-      this.children.push(new Circle(this.r, level + 1));
-    } // for k
-  } // if (this.level <= 4)
+// Base camera
+const camera = new THREE.PerspectiveCamera(
+75,
+sizes.width / sizes.height,
+0.1,
+100);
 
-} // Circle
+camera.position.set(1, 1, 1);
+scene.add(camera);
 
-Circle.prototype.draw = function(xc, yc) {
-  ctx.beginPath();
-  if (this.hue0 === false)
-    ctx.fillStyle = '#000';
-  else {
-    ctx.fillStyle = `hsl(${hue},100%,${this.fdLum()}%)`;
-  }
-  ctx.arc(xc, yc, this.R, 0, m2PI);
-  ctx.fill();
-  if (this.children.length > 0) {
-    this.th0 = (this.th0 + this.fdth()) % m2PI;
-    for (let k = 0; k < this.n; ++k) {
-      let ang = this.th0 + k * m2PI / this.n;
-      this.children[k].draw(xc + this.dr * mcos(ang), yc + this.dr * msin(ang));
-    } // let k
+// Controls
+const controls = new THREE.OrbitControls(camera, canvas);
+controls.enableDamping = true;
 
-  } // if this.children.length > 0
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas });
 
-} // Circle.prototype.draw
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
-//------------------------------------------------------------------------
+/**
+ * Add GUI
+ */
+// Big Waves
+bigWavesFolder.
+add(waterMaterial.uniforms.uBigWavesElevation, "value").
+min(0).
+max(1).
+step(0.001).
+name("Elevation");
+bigWavesFolder.
+add(waterMaterial.uniforms.uBigWavesFrequency.value, "x").
+min(0).
+max(10).
+step(0.001).
+name("Frequency X");
+bigWavesFolder.
+add(waterMaterial.uniforms.uBigWavesFrequency.value, "y").
+min(0).
+max(10).
+step(0.001).
+name("Frequency Y");
+bigWavesFolder.
+add(waterMaterial.uniforms.uBigWaveSpeed, "value").
+min(0.25).
+max(5).
+step(0.001).
+name("Speed");
 
-let animate;
+// Small Waves
+smallWavesFolder.
+add(waterMaterial.uniforms.uSmallWavesElevation, "value").
+min(0.0).
+max(0.3).
+step(0.001).
+name("Elevation");
+smallWavesFolder.
+add(waterMaterial.uniforms.uSmallWavesFrequency, "value").
+min(0).
+max(30).
+step(0.001).
+name("Frequency");
+smallWavesFolder.
+add(waterMaterial.uniforms.uSmallWavesSpeed, "value").
+min(0.0).
+max(1).
+step(0.001).
+name("Speed");
+smallWavesFolder.
+add(waterMaterial.uniforms.uSmallWavesIterations, "value").
+min(0).
+max(10).
+step(1).
+name("Iterations");
 
-{ // scope for animate
+// Colors
+colorFolder.
+add(waterMaterial.uniforms.uColorOffset, "value").
+min(0).
+max(0.15).
+step(0.0001).
+name("Color Offset");
+colorFolder.
+add(waterMaterial.uniforms.uColorMultiplier, "value").
+min(0.0).
+max(10.0).
+step(0.001).
+name("Color multiplier");
+colorFolder.
+addColor(debugObject, "waveDepthColor").
+name("Wave depth color").
+onChange(() => {
+  waterMaterial.uniforms.uDepthColor.value.set(debugObject.waveDepthColor);
+});
+colorFolder.
+addColor(debugObject, "waveSurfaceColor").
+name("Wave surface color").
+onChange(() => {
+  waterMaterial.uniforms.uSurfaceColor.value.set(
+  debugObject.waveSurfaceColor);
 
-let animState = 0;
+});
+colorFolder.
+addColor(debugObject, "fogColor").
+name("Fog Color").
+onChange(() => {
+  waterMaterial.uniforms.fogColor.value.set(debugObject.fogColor);
+  scene.background.set(debugObject.fogColor);
+  scene.fog = new THREE.Fog(
+  debugObject.fogColor,
+  debugObject.fogNear,
+  debugObject.fogFar);
 
-animate = function(tStamp) {
+});
 
-  let event;
-  let tinit;
+/**
+ * Animate
+ */
+const clock = new THREE.Clock();
 
-  event = events.pop();
-  if (event && event.event == 'reset') animState = 0;
-  if (event && event.event == 'click') animState = 0;
-  window.requestAnimationFrame(animate)
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime();
 
-  switch (animState) {
+  // Update controls
+  controls.update();
 
-    case 0 :
-      if (startOver()) {
-        ++animState;
-      }
-      break;
+  // Update time
+  waterMaterial.uniforms.uTime.value = elapsedTime;
 
-    case 1 :
-      c.draw(maxx / 2, maxy / 2);
-      break;
+  // Render
+  renderer.render(scene, camera);
 
-    case 2:
-      break;
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick);
+};
 
-  } // switch
-
-} // animate
-} // scope for animate
-
-//------------------------------------------------------------------------
-//------------------------------------------------------------------------
-
-function startOver() {
-
-// canvas dimensions
-
-  maxx = window.innerWidth;
-  maxy = window.innerHeight;
-
-  canv.width = maxx;
-  canv.height = maxy;
-  ctx.lineJoin = 'round';
-  ctx.lineCap = 'round';
-
-  ctx.fillStyle = '#000';
-  ctx.fillRect(0,0,maxx,maxy);
-
-  hue = intAlea(360);
-  c = new Circle (mmin( maxx - 10, maxy -10) / 2, 0);
-
-  return true;
-
-} // startOver
-
-//------------------------------------------------------------------------
-
-function mouseClick (event) {
-
-  events.push({event:'click'});;
-
-} // mouseClick
-
-//------------------------------------------------------------------------
-//------------------------------------------------------------------------
-// beginning of execution
-
-  {
-    canv = document.createElement('canvas');
-    canv.style.position="absolute";
-    document.body.appendChild(canv);
-    ctx = canv.getContext('2d');
-    canv.setAttribute ('title','click me');
-  } // création CANVAS
-  canv.addEventListener('click',mouseClick);
-  events = [{event:'reset'}];
-  requestAnimationFrame (animate);
-
-}); // window load listener
+tick();
